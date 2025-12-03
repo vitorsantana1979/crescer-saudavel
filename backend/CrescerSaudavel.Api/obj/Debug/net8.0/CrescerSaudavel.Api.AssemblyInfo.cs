6601b47e72fed0591ad6d4c99c9288676f8688ef
vitorsantana1979 <vitorsantanal@gmail.com>
2025-12-03 11:29:29 -0300

    feat: Configurações completas de produção (Dockerfile frontend, Caddyfile, instruções IA)

--- backend/CrescerSaudavel.Api/obj/Debug/net8.0/CrescerSaudavel.Api.AssemblyInfo.cs
+++ backend/CrescerSaudavel.Api/obj/Debug/net8.0/CrescerSaudavel.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrescerSaudavel.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22a28069f873db28cc54101da654c160778c2944")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a057535bd912bb41da95693b9fff8ad282323d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrescerSaudavel.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrescerSaudavel.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
